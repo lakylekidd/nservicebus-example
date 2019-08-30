a18beeb4388cc373593906fcc4b9fe7ad808c50c
Billy Vlachos <williamcaminiti@live.com>
2019-08-30 15:29:38 +0200

    Create order command

---
+++ Orders/Commands/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Orders.Commands
+{
+    public class Command
+    {
+        public Guid OrderId { get; }
+
+        public Command(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
--- Orders/Commands/PlaceOrderCommand.cs
+++ Orders/Commands/PlaceOrderCommand.cs
@@ -1,6 +1,15 @@
-﻿namespace Orders.Commands
+﻿using System;
+
+namespace Orders.Commands
 {
-    public class PlaceOrderCommand
+    public class PlaceOrderCommand : Command
     {
+        public DateTime OrderDate { get; }
+
+        public PlaceOrderCommand(Guid id)
+            :base(id)
+        {
+            OrderDate = DateTime.Now;
+        }
     }
 }
