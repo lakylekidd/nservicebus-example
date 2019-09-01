c001aa271e32b3eb309aa8db62e4b96cd508e0ee
Billy Vlachos <williamcaminiti@live.com>
2019-09-01 11:35:09 +0200

    Include nservicebus in startup

--- Payments/Handlers/OrderPlacedEventHandler.cs
+++ Payments/Handlers/OrderPlacedEventHandler.cs
@@ -1,11 +1,19 @@
 ﻿using System.Threading.Tasks;
 using Core.Messages.Events;
 using NServiceBus;
+using Payments.Infrastructure;
 
 namespace Payments.Handlers
 {
     public class OrderPlacedEventHandler : IHandleMessages<OrderPlacedEvent>
     {
+        IPaymentRepository _paymentRepository;
+
+        public OrderPlacedEventHandler(IPaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+
         public Task Handle(OrderPlacedEvent message, IMessageHandlerContext context)
         {
             throw new System.NotImplementedException();
--- Payments/Startup.cs
+++ Payments/Startup.cs
@@ -10,11 +10,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NServiceBus;
+using NServiceBus.ObjectBuilder.MSDependencyInjection;
+using Payments.Infrastructure;
 
 namespace Payments
 {
     public class Startup
     {
+        IEndpointInstance endpoint;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -23,9 +28,36 @@ namespace Payments
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSingleton<IPaymentRepository, PaymentRepository>();
+            services.AddSingleton(sp => endpoint);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Configure NServiceBus
+            var endpointConfiguration = new EndpointConfiguration("Assignment.Orders");
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+            endpointConfiguration.SendFailedMessagesTo("failed");
+            endpointConfiguration.AuditProcessedMessagesTo("audit");
+            endpointConfiguration.SendHeartbeatTo("Assignment.ServiceControl");
+            var metrics = endpointConfiguration.EnableMetrics();
+            metrics.SendMetricDataToServiceControl("Assignment.Monitoring", TimeSpan.FromMilliseconds(500));
+
+            UpdateableServiceProvider container = null;
+            endpointConfiguration.UseContainer<ServicesBuilder>(customizations =>
+            {
+                customizations.ExistingServices(services);
+                customizations.ServiceProviderFactory(sc =>
+                {
+                    container = new UpdateableServiceProvider(sc);
+                    return container;
+                });
+            });
+
+            endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+            return container;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
