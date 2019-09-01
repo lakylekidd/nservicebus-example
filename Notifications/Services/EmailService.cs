031fef358174c73e4c3c1d328291d7dde2ad325d
Billy Vlachos <williamcaminiti@live.com>
2019-09-01 12:12:40 +0200

    Add notification service interface and resolver for DI

---
+++ Notifications/Services/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Notifications.Domain;
+
+namespace Notifications.Services
+{
+    public class EmailService : INotificationService
+    {
+        public Task Send(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Notifications/Services/SMSService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Notifications.Domain;
+
+namespace Notifications.Services
+{
+    public class SMSService : INotificationService
+    {
+        public Task Send(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
