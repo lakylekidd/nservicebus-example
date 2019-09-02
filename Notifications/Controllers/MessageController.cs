1c3cd17d1c55046725fffae3faac6262492658cc
Billy Vlachos <williamcaminiti@live.com>
2019-09-02 13:26:46 +0200

    Create controller to return all messages

---
+++ Notifications/Controllers/MessageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notifications.Infrastructure;
+using Payments.Infrastructure;
+using System.Threading.Tasks;
+
+namespace Payments.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        IMessageRepository _messageRepository;
+
+        public MessagesController(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult> GetMessages()
+        {
+            var messages = await _messageRepository.GetAll();
+            return Ok(messages);
+        }
+
+    }
+}
